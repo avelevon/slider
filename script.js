8ea14bfb1b5ea84dd6fbb817e048d88bfc2e5a10
Levon Avetisyan <levon05@inbox.ru>
2019-02-10 21:00:53 +0300

    slider with various number of slides

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
+//moving distance
 let dist = 300;
+
 
 
 function slider(cycle, direction) {
@@ -8,44 +10,34 @@ function slider(cycle, direction) {
         let position = dist * (-2);
         $('.single-slider').animate({
             left: position,
-        }, 2000);
-        if (cycle === 1) {
-            $('.single-slider').promise().done(function () {
-                $('.single-slider:last').after($('.single-slider[data-id="5"]'));
-                position += dist;
-                $('.single-slider').css('left', position + 'px');
-            })
-        } else {
-            $('.single-slider').promise().done(function () {
-                $('.single-slider:last').after($('.single-slider[data-id="' + (cycle - 2) + '"]'));
-                position += dist;
-                $('.single-slider').css('left', position + 'px');
-            })
-        }
+        }, 500);
+        $('.single-slider').promise().done(function () {
+            $('.single-slider:last').after($('.single-slider[data-id="' + (cycle - 1) + '"]'));
+            position += dist;
+            $('.single-slider').css('left', position + 'px');
 
+            $('#left-click').addClass('left-click');
+        })
     } else if (direction === 'right') {
         let position = 0;
         $('.single-slider').animate({
             left: position,
-        }, 2000);
-        if (cycle === 10) {
-            $('.single-slider').promise().done(function () {
-                $('.single-slider:first').before($('.single-slider[data-id="1"]'));
-                position -= dist;
-                $('.single-slider').css('left', position + 'px');
-            })
-        } else {
-            $('.single-slider').promise().done(function () {
-                $('.single-slider:first').before($('.single-slider[data-id="' + (cycle - 1) + '"]'));
-                position -= dist;
-                $('.single-slider').css('left', position + 'px');
-            })
-        }
+        }, 500);
+        $('.single-slider').promise().done(function () {
+            $('.single-slider:first').before($('.single-slider[data-id="' + (cycle - 1) + '"]'));
+            position -= dist;
+            $('.single-slider').css('left', position + 'px');
+
+            $('#right-click').addClass('right-click');
+        })
     }
 }
 
 (function ($) {
     $(function () {
+        //number of slides
+
+        let slidesNumber = $('.single-slider').length;
 
         let cycle = 1;
         $('#cycle').text(cycle);
@@ -60,23 +52,25 @@ function slider(cycle, direction) {
 
         $('#buttons-wrapper').on('click', '.left-click', function (event) {
             event.preventDefault();
-            if (cycle === 6) {
+            $('#left-click').removeClass('left-click');
+            slider(cycle, 'left');
+            if (cycle === slidesNumber) {
                 cycle = 1;
             } else {
                 cycle += 1;
             }
             $('#cycle').text(cycle);
-            slider(cycle, 'left');
         });
 
         $('#buttons-wrapper').on('click', '.right-click', function (event) {
             event.preventDefault();
             if (cycle === 1) {
-                cycle = 6;
+                cycle = slidesNumber;
             } else {
                 cycle -= 1;
             }
             $('#cycle').text(cycle);
+            $('#right-click').removeClass('right-click');
             slider(cycle, 'right');
         });
 
